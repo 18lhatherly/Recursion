17794ea5632a2ebfe6f9cbd60faaa910eaea6b46
Laura Hatherly <18LHatherly@heckgrammar.co.uk>
2024-02-05 09:53:45 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(8000));
+            Console.ReadLine();
+        }
+        static BigInteger Factorial(int n)
+        {
+            if(n == 0 || n == 1)
+            {
+                return 1;
+            }else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
